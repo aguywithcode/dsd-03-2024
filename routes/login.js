781eccd9f66a2deb8f4278d6dafeda8ca94ef6f0
Sandra Chaparro <sandra.saldepaz@gmail.com>
2024-04-08 19:12:26 -0500

    added login component/endpoint

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* Login page for user. */
 router.get('/', function(req, res, next) {
-  res.send('This will be the login page');
+  res.sendFile(path.join(__dirname, '../view/public', 'index.html'));
 });
 
 export default router;
--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState, useEffect} from 'react';
 
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
 
 
 function App() {
@@ -20,22 +22,14 @@ function App() {
   }, []);
 
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {data.name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} /> 
+      </Routes>
     </div>
+  </Router>
   );
 }
 
